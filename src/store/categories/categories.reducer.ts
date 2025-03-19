45e49c4d423d9e7542c9e97bd05f29c02fe1bb29
Benjamin Corbett <benjamincorbett@Benjamins-iMac.local>
2025-03-19 14:13:40 -0400

    typing redux categories

--- src/store/categories/categories.reducer.ts
+++ src/store/categories/categories.reducer.ts
@@ -1,5 +1,10 @@
-import { CategoryAction } from "./categories.action";
-import { CATEGORIES_ACTION_TYPES, Category } from "./categories.types";
+import { AnyAction } from "redux";
+import {
+  fetchCategoriesFailed,
+  fetchCategoriesStart,
+  fetchCategoriesSuccess,
+} from "./categories.action";
+import { Category } from "./categories.types";
 
 export type CategoriesState = {
   readonly categories: Category[];
@@ -15,16 +20,19 @@ export const CATEGORIES_INITIAL_STATE = {
 
 export const categoriesReducer = (
   state = CATEGORIES_INITIAL_STATE,
-  action = {} as CategoryAction //discriminating union very common patter in TS Redux Projects
-) => {
-  switch (action.type) {
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
-      return { ...state, isLoading: true };
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
-      return { ...state, categories: action.payload, isLoading: false };
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
-      return { ...state, error: action.payload, isLoading: false };
-    default:
-      return state;
+  action = {} as AnyAction //discriminating union very common patter in TS Redux Projects
+): CategoriesState => {
+  if (fetchCategoriesStart.match(action)) {
+    return { ...state, isLoading: true };
   }
+
+  if (fetchCategoriesSuccess.match(action)) {
+    return { ...state, categories: action.payload, isLoading: false };
+  }
+
+  if (fetchCategoriesFailed.match(action)) {
+    return { ...state, error: action.payload, isLoading: false };
+  }
+
+  return state;
 };
--- src/utils/firebase/reducer/reducer.utils.ts
+++ src/utils/firebase/reducer/reducer.utils.ts
@@ -1,23 +1,50 @@
+import { AnyAction } from "redux";
+
+// Rather Advanced Also What is the new "Any Action"
+//WithMatcher Main Idea: We are tryign to extract the type off of the action that comes off of this action creator and using these to do double duty so that Action creator functions can match actions off of our reducer.
+type Matchable<AC extends () => AnyAction> = AC & {
+  type: ReturnType<AC>["type"];
+  match(action: AnyAction): action is ReturnType<AC>;
+};
+
+export function withMatcher<AC extends () => AnyAction & { type: string }>(
+  actionCreator: AC
+): Matchable<AC>;
+
+export function withMatcher<
+  AC extends (...args: any[]) => AnyAction & { type: string }
+>(actionCreator: AC): Matchable<AC>;
+
+export function withMatcher(actionCreator: Function) {
+  const type = actionCreator().type;
+  return Object.assign(actionCreator, {
+    type,
+    match(action: AnyAction) {
+      return action.type === type;
+    },
+  });
+}
+
 export type ActionWithPayload<T, P> = {
   type: T;
   payload: P;
 };
 
 // You want to make sure you define actions with and without a payload
-export type Action<T> {
-  type: T
+export type Action<T> = {
+  type: T;
 };
 
-export function createAction<T extends string,  P>(type: T, payload: P): ActionWithPayload<T, P>
+export function createAction<T extends string, P>(
+  type: T,
+  payload: P
+): ActionWithPayload<T, P>;
 
-export function createAction<T extends string>(type: T, payload: void): Action <T>
+export function createAction<T extends string>(
+  type: T,
+  payload: void
+): Action<T>;
 
 export function createAction<T extends string, P>(type: T, payload: P) {
-  return { type, payload}
+  return { type, payload };
 }
-// export const createAction = (type, payload) => ({
-//   type,
-//   payload,
-// });
-
-
